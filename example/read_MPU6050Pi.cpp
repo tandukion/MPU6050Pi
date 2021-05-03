333fb65eced50c3679c33210ec111ea916609fab
dwindra <tandukion@hotmail.com>
2021-05-03 06:31:13 -0400

    Added read MPU example with calibration file

--- example/read_MPU6050Pi.cpp
+++ example/read_MPU6050Pi.cpp
@@ -1,12 +1,64 @@
 #include <iostream>	
 #include <iomanip>
+#include <fstream>
+#include <string.h>
+
 
 #include <MPU6050Pi.h>
 
 int main(int argc, char **argv) {
-
-    // Connect to device.
+    // Connect to device with default setting
     MPU6050Pi mpu;
+
+    if (argc > 1){
+        int16_t offsets[6];
+
+        // Getting the file extension
+        char *p = strrchr(argv[1], '.');
+        if (p) {
+            // Make sure the calibration file is CSV
+            if (strcmp(p, ".csv") == 0) {
+                std::ifstream ifile (argv[1]);
+                std::string header;
+                std::string data_str;
+                if (ifile.is_open()){
+                    // Getting data line by line
+                    // 1st line should be Header
+                    std::getline(ifile,header);
+
+                    // Data
+                    std::getline(ifile,data_str);
+
+                    ifile.close();
+
+                    // Parse csv
+                    int i=0;
+                    size_t pos = 0;
+                    std::string token;
+                    std::string delimiter = ",";
+                    while ((pos = data_str.find(delimiter)) != std::string::npos) {
+                        offsets[i] = stoi(data_str.substr(0, pos));
+                        data_str.erase(0, pos + delimiter.length());
+                        i++;
+                    }
+                    // Handle last data if not ending with comma
+                    if (data_str.length() > 0){
+                        offsets[i] = stoi(data_str);
+                    }
+                }
+            }
+            else {
+                std::cout << "Wrong input file" << std::endl;
+                return 0;
+            }
+        }
+        else {
+            std::cout << "Wrong input file" << std::endl;
+            return 0;
+        }
+
+        mpu.SetOffset(offsets);
+    }
 
     int16_t ax, ay, az, gx, gy, gz;
 
