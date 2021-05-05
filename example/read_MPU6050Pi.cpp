8f39a43979e02d5e4b46a12ff0e6cd22c8bade1c
dwindra <tandukion@hotmail.com>
2021-05-05 05:04:06 -0400

    Made reading calibration file into function

--- example/read_MPU6050Pi.cpp
+++ example/read_MPU6050Pi.cpp
@@ -6,57 +6,65 @@
 
 #include <MPU6050Pi.h>
 
-int main(int argc, char **argv) {
-    // Connect to device with default setting
-    MPU6050Pi mpu;
+int16_t* GetCalibrationData (char *filename) {
+    int16_t *offsets = new int16_t[6];
 
-    if (argc > 1){
-        int16_t offsets[6];
+    // Getting the file extension
+    char *file_ext = strrchr(filename, '.');
+    if (file_ext) {
+        // offsets = GetCalibrationData(argv[1]);
+        // Make sure the calibration file is CSV
+        if (strcmp(file_ext, ".csv") == 0) {
+            std::ifstream ifile (filename);
+            std::string header;
+            std::string data_str;
+            if (ifile.is_open()){
+                // Getting data line by line
+                // 1st line should be Header
+                std::getline(ifile,header);
 
-        // Getting the file extension
-        char *p = strrchr(argv[1], '.');
-        if (p) {
-            // Make sure the calibration file is CSV
-            if (strcmp(p, ".csv") == 0) {
-                std::ifstream ifile (argv[1]);
-                std::string header;
-                std::string data_str;
-                if (ifile.is_open()){
-                    // Getting data line by line
-                    // 1st line should be Header
-                    std::getline(ifile,header);
+                // Data
+                std::getline(ifile,data_str);
 
-                    // Data
-                    std::getline(ifile,data_str);
+                ifile.close();
 
-                    ifile.close();
-
-                    // Parse csv
-                    int i=0;
-                    size_t pos = 0;
-                    std::string token;
-                    std::string delimiter = ",";
-                    while ((pos = data_str.find(delimiter)) != std::string::npos) {
-                        offsets[i] = stoi(data_str.substr(0, pos));
-                        data_str.erase(0, pos + delimiter.length());
-                        i++;
-                    }
-                    // Handle last data if not ending with comma
-                    if (data_str.length() > 0){
-                        offsets[i] = stoi(data_str);
-                    }
+                // Parse csv
+                int i=0;
+                size_t pos = 0;
+                std::string token;
+                std::string delimiter = ",";
+                while ((pos = data_str.find(delimiter)) != std::string::npos) {
+                    offsets[i] = stoi(data_str.substr(0, pos));
+                    data_str.erase(0, pos + delimiter.length());
+                    i++;
+                }
+                // Handle last data if not ending with comma
+                if (data_str.length() > 0){
+                    offsets[i] = stoi(data_str);
                 }
             }
-            else {
-                std::cout << "Wrong input file" << std::endl;
-                return 0;
-            }
+            return offsets;
         }
         else {
             std::cout << "Wrong input file" << std::endl;
             return 0;
         }
+    }
+    else {
+        std::cout << "Wrong input file" << std::endl;
+        return 0;
+    }
+}
 
+int main(int argc, char **argv) {
+    // Connect to device with default setting
+    MPU6050Pi mpu;
+
+    if (argc > 1){
+        int16_t *offsets;
+
+        offsets = GetCalibrationData(argv[1]);
+        if (!offsets) return 0;
         mpu.SetOffset(offsets);
     }
 
