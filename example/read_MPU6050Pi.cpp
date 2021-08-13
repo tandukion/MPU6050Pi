af330fa5ab486597119add5270773f5815096f90
dwindra <tandukion@hotmail.com>
2021-08-13 19:41:41 +0900

    Added example to use DMP with this library

--- example/read_MPU6050Pi.cpp
+++ example/read_MPU6050Pi.cpp
@@ -74,13 +74,29 @@ int main(int argc, char **argv) {
 
     int16_t ax, ay, az, gx, gy, gz;
     float accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z;
-    int devStatus;
+
+    // DMP variables
+    int dev_status;
+    uint8_t mpu_int_status;
+    uint16_t packet_size;
+    uint16_t fifo_count;
+    uint8_t fifo_buffer[64];
+    Quaternion q;
+    Vector gravity;
+    float euler[3];
+    float ypr[3]; // yaw pitch roll
+    float yaw, pitch, roll;
+    float psi, theta, phi;
+
+    //
+    uint8_t i,j;
 
     std::cout << "----------------------------" << std::endl;
     std::cout << "1. Raw Sensor Data (int)" << std::endl;
     std::cout << "2. Float Sensor Data" << std::endl;
     std::cout << "3. Orientation (Complementary filter)" << std::endl;
-    std::cout << "4. DMP" << std::endl;
+    std::cout << "4. DMP (Yaw, Pitch, Roll)" << std::endl;
+    std::cout << "5. DMP (Euler angles)" << std::endl;
     std::cout << "----------------------------" << std::endl;
     std::cout << "Choose mode to choose: ";
     
@@ -135,25 +151,96 @@ int main(int argc, char **argv) {
             break;
         
         case 4:
+        case 5:
             std::cout << "Initalize DMP..\n";
-            devStatus = mpu.DMPInitalize();
+            dev_status = mpu.DMPInitalize();
 
             // make sure it worked (returns 0 if so)
-            if (devStatus == 0) {
+            if (dev_status == 0) {
                 // turn on the DMP, now that it's ready
                 std::cout << "Enabling DMP...\n";
                 mpu.SetDMPEnabled(true);
 
                 // get expected DMP packet size for later comparison
-                // packetSize = mpu.dmpGetFIFOPacketSize();
+                packet_size = mpu.DMPGetFIFOPacketSize();
+                std::cout << "Packet size: " << (int)packet_size << std::endl;
             } else {
                 // ERROR!
                 // 1 = initial memory load failed
                 // 2 = DMP configuration updates failed
                 // (if it's going to break, usually the code will be 1)
                 std::cout << "DMP Initialization failed.\n Error code: ";
-                std::cout << devStatus << "\n";
-            } 
+                std::cout << dev_status << "\n";
+                break;
+            }
+
+            // Publish in loop.
+            std::cout << "DMP Data\n";
+            std::cout << std::fixed << std::setprecision(6) << std::setfill(' ');
+            if (mode==4)
+                std::cout << std::setw(12) << "yaw(deg)" << std::setw(12) << "pitch(deg)" << std::setw(12) << "roll(deg)";
+            if (mode==5)
+                std::cout << std::setw(12) << "psi(deg)" << std::setw(12) << "theta(deg)" << std::setw(12) << "phi(deg)";
+            std::cout << std::endl;
+            while(1) {
+                // Clear the buffer so as we can get fresh values
+                // The sensor is running a lot faster than our sample period
+                mpu.ResetFIFO();
+                
+                // Waiting until FIFO full on MPU6050 side
+                // std::cout << "Waiting until FIFO full...\n";
+                while (fifo_count < packet_size) {
+                    fifo_count = mpu.GetFIFOCount();
+                    // std::cout << "FIFO count: " << fifo_count << "\n";
+                }
+                if (fifo_count == 1024) {
+                    mpu.ResetFIFO();
+                    std::cout << "FIFO overflow!\n";
+                    break;
+                }
+                else{
+                    // std::cout << "FIFO ready! FIFO count: " << fifo_count << "\n";
+
+                    // Read FIFO buffer
+                    while (fifo_count >= packet_size) {
+                        mpu.GetFIFOBytes(fifo_buffer,packet_size);
+                        fifo_count -= packet_size;
+
+                        // Debug
+                        // std::cout << "FIFO count remaining: " << fifo_count << "\n";
+                        // std::cout << "FIFO buffer: \n";
+                        // for (j=0; j<(sizeof(fifo_buffer)/sizeof(*fifo_buffer));) {
+                        //     std::cout << std::hex << std::setfill('0') << std::setw(2) << (uint16_t) fifo_buffer[j] << " ";
+                        //     j++;
+                        //     if (j%16 == 0) std::cout << std::endl;
+                        // }
+                    }
+
+                    // Process FIFO buffer
+                    mpu.DMPGetQuaternion(&q, fifo_buffer);
+
+                    if (mode==4){
+                        mpu.DMPGetGravity(&gravity, &q);
+                        mpu.DMPGetYawPitchRoll(ypr, &q, &gravity);
+                        yaw = ypr[0] * 180/M_PI;
+                        pitch = ypr[1] * 180/M_PI;
+                        roll = ypr[2] * 180/M_PI;
+
+                        std::cout << std::setw(12) << ypr[0] << std::setw(12) << ypr[1] << std::setw(12) << ypr[2];
+                        std::cout << "\r";
+                    }
+                    else if (mode==5) {
+                        mpu.DMPGetEuler(euler, &q);
+                        psi = euler[0] * 180/M_PI;
+                        theta = euler[1] * 180/M_PI;
+                        phi = euler[2] * 180/M_PI;
+                        
+                        std::cout << std::setw(12) << psi << std::setw(12) << theta << std::setw(12) << phi;
+                        std::cout << "\r";
+                    }
+                }
+            }
+
 
             break;
 
